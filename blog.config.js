b4c948b1663054f38c0e12f2276745e37e5513ce
DESKTOP-074HU7F\Luke <lukegrech2002@gmail.com>
2022-04-03 17:31:18 +0200

    first commit

--- blog.config.js
+++ blog.config.js
@@ -15,8 +15,8 @@ const BLOG = {
   sortByDate: false,
   showArchive: true,
   autoCollapsedNavBar: false, // The automatically collapsed navigation bar
-  // ogImageGenerateURL: 'https://og-image-craigary.vercel.app', // The link to generate OG image, don't end with a slash
-  socialLink: "https://twitter.com/craigaryhart",
+  // ogImageGenerateURL: '', // The link to generate OG image, don't end with a slash
+  socialLink: "https://instagram.com/lukegrc",
   seo: {
     keywords: ["Blog", "Website", "Notion"],
     googleSiteVerification: "", // Remove the value or replace it with your own google site verification code
@@ -26,8 +26,8 @@ const BLOG = {
   analytics: {
     provider: "", // Currently we support Google Analytics and Ackee, please fill with 'ga' or 'ackee', leave it empty to disable it.
     ackeeConfig: {
-      tracker: "", // e.g 'https://ackee.craigary.net/tracker.js'
-      dataAckeeServer: "", // e.g https://ackee.craigary.net , don't end with a slash
+      tracker: "", // e.g 'https://ackee.test.net/tracker.js'
+      dataAckeeServer: "", // e.g https://ackee.test.net , don't end with a slash
       domainId: "", // e.g '0e2257a8-54d4-4847-91a1-0311ea48cc7b'
     },
     gaConfig: {
@@ -56,5 +56,5 @@ const BLOG = {
   },
   isProd: process.env.VERCEL_ENV === "production", // distinguish between development and production environment (ref: https://vercel.com/docs/environment-variables#system-environment-variables)
 };
-// export default BLOG
+
 module.exports = BLOG;
